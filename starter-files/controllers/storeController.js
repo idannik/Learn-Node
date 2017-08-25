0ff19270fc5970be4e3cdb3fc11d8ceb3aca72aa
Idan Nikritin <idan.nik@gmail.com>
2017-08-25 13:04:11 +0300

    add createStore method

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+const Store =mongoose.model('Store');
 exports.homePage = (req, res) => {
       console.log("Hey!!!")
 res.render("index", {
@@ -11,6 +13,8 @@ exports.addStore = (req, res) => {
   res.render("editStore", {title : "add store"})
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body)
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);  
+  await store.save();
+  res.redirect('/')
 }
