dd5b2e94d93bdd8159f50a772f3d1f2599d40253
Idan Nikritin <idan.nik@gmail.com>
2017-09-02 16:13:40 +0300

    Login mechanism

---
+++ starter-files/controllers/userController.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+const User = mongoose.model("User")
+const prmoisify = require("es6-promisify")
+
+exports.loginForm =  (req, res) => {
+    res.render('login', {title: 'Login'})
+}
+
+exports.registerForm = (req, res) => {
+    res.render('register', {title: 'Register'})
+}
+
+exports.validateRegister = (req, res, next) => {
+    req.sanitizeBody("name")
+    req.checkBody("name", "You must submit a name!").notEmpty()
+    req.checkBody("email", "That email is not valid!").isEmail();
+    req.sanitizeBody("email").normalizeEmail({
+        remove_dots: false,
+        remove_extension: false,
+        gmail_remove_subaddress: false
+    });
+    req.checkBody("password", 'Password must not be blank').notEmpty();
+    req.checkBody("password-confirm", 'Confirm Password must not be blank').notEmpty();
+    req.checkBody("password-confirm", "Ooops 😅, your passwords do not match").equals(req.body.password)
+    const errors = req.validationErrors();
+    if (errors) {
+        req.flash('error', errors.map(err => err.msg ))
+        res.render('register', {title : "register", body: req.body, flashes:req.flash()})
+        return;
+    }
+    next()
+    
+};
+
+exports.register =  async (req, res, next) => {
+    const user = new User({email: req.body.email,
+                        password: req.body.password,
+                        name: req.body.name})
+    const registerWithPromise = prmoisify(User.register, User);
+    await registerWithPromise(user, req.body.password);
+    next()
+}
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const storeController = require("../controllers/storeController")
-const {catchErrors} = require('../handlers/errorHandlers')
+const storeController = require("../controllers/storeController");
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const {catchErrors} = require('../handlers/errorHandlers');
+
 
 //gets
 router.get('/', catchErrors(storeController.getStores))
@@ -15,7 +19,18 @@ router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug))
 router.get('/tags/', catchErrors(storeController.getStoresByTag))
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag))
 
+router.get("/login", userController.loginForm);
+router.get("/register", userController.registerForm);
+
+
 //post/update
+
+router.post("/register", 
+    userController.validateRegister,
+    userController.register,
+    authController.login);
+
+
 router.post('/add', 
     storeController.upload,
     catchErrors(storeController.resize),
